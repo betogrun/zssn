3f37786bb7814dcb025bd76dc53993ae946c5045
Alberto Rocha <betogrun@gmail.com>
2016-12-07 21:07:24 -0200

    Add feature to mark a survivor as infected

--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -2,4 +2,13 @@ class Survivor < ApplicationRecord
   enum gender: [:male, :female]
   has_many :items
   has_one :location
+
+  attr_accessor :complaints, :is_infected
+
+  def increment_complaints
+    @complaints += 1
+    if @complaints >= 3
+      @is_infected = true
+    end
+  end
 end
