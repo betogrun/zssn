c2821c4e657d2c590b6aaaadef45abf43e0a3123
Alberto Rocha <betogrun@gmail.com>
2016-12-10 20:15:00 -0200

    Update routes and routing tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-
-  resources :denouncements
   resources :survivors, except: [:update, :destroy] do
       resources :locations
       resources :items
+      resources :denouncements, except: [:update, :destroy]
   end
 end
--- spec/routing/denouncements_routing_spec.rb
+++ spec/routing/denouncements_routing_spec.rb
@@ -4,35 +4,15 @@ RSpec.describe DenouncementsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/denouncements").to route_to("denouncements#index")
-    end
-
-    it "routes to #new" do
-      expect(:get => "/denouncements/new").to route_to("denouncements#new")
+      expect(:get => "/survivors/1/denouncements").to route_to("denouncements#index", :survivor_id => "1" )
     end
 
     it "routes to #show" do
-      expect(:get => "/denouncements/1").to route_to("denouncements#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/denouncements/1/edit").to route_to("denouncements#edit", :id => "1")
+      expect(:get => "/survivors/1/denouncements/1").to route_to("denouncements#show", :id => "1", :survivor_id => "1" )
     end
 
     it "routes to #create" do
-      expect(:post => "/denouncements").to route_to("denouncements#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(:put => "/denouncements/1").to route_to("denouncements#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/denouncements/1").to route_to("denouncements#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/denouncements/1").to route_to("denouncements#destroy", :id => "1")
+      expect(:post => "/survivors/1/denouncements").to route_to("denouncements#create", :survivor_id => "1")
     end
 
   end
