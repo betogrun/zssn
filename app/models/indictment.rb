52b242e9ad6680e78f1cfced418f9f32a17bf50f
Alberto Rocha <betogrun@gmail.com>
2016-12-10 17:59:27 -0200

    Remove indicment entity

--- app/models/indictment.rb
+++
@@ -1,4 +0,0 @@
-class Indictment < ApplicationRecord
-  belongs_to :accuser, class_name: 'Survivor'
-  belongs_to :indicted, class_name: 'Survivor'
-end
--- db/migrate/20161208032409_create_indictments.rb
+++
@@ -1,9 +0,0 @@
-class CreateIndictments < ActiveRecord::Migration[5.0]
-  def change
-    create_table :indictments do |t|
-      t.integer :accuser_id
-      t.integer :indicted_id
-      t.timestamps
-    end
-  end
-end
