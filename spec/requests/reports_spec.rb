efd44ccb851a0fd6f58a2c0656496a6a004d2449
Alberto Rocha <betogrun@gmail.com>
2016-12-12 09:41:03 -0200

    Update reports tests

--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Reports", type: :request do
   describe "GET /reports" do
     it "show the reports" do
+      FactoryGirl.create(:survivor)
       get reports_path
       expect(response).to have_http_status(200)
     end
