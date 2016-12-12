9d19c657dc2868ddf377529cc7dc177db24a435c
Alberto Rocha <betogrun@gmail.com>
2016-12-12 09:31:16 -0200

    Update report model, controller and tests

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,36 @@
 class Report < ApplicationRecord
+  attr_accessor :total
+  def self.run
+    @total = Survivor.count
+   "Infected users:#{infected_users},
+                  Not Infected users: #{not_infected_users},
+                  Average amount of water: #{average_amount(:water)},
+                  Average amount of food: #{average_amount(:food)},
+                  Average amount of medicine: #{average_amount(:medicine)},
+                  Average amount of ammo: #{average_amount(:ammo)},
+                  Lost score per zombie: #{lost_score}"
+  end
+
+  private
+
+  def self.infected_users
+    Survivor.where(is_infected: true).count.to_f / @total * 100
+  end
+
+  def self.not_infected_users
+    100 - infected_users
+  end
+
+  def self.average_amount(kind)
+    Item.where(kind: kind).reduce(0) { |sum, i| i.amount } / @total
+  end
+
+  def self.lost_score
+    infected_items = []
+    Survivor.where(is_infected: true).each do |survivor|
+      infected_items = Item.where(survivor_id: survivor.id)
+    end
+    Barter.balance(infected_items)
+  end
+
 end
--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Reports", type: :request do
   describe "GET /reports" do
-    it "works! (now write some real specs)" do
+    it "show the reports" do
       get reports_path
       expect(response).to have_http_status(200)
     end
