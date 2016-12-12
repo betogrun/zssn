dc01db7438c8b8075430c3d26dd1c0c8f2b6afce
Alberto Rocha <betogrun@gmail.com>
2016-12-12 09:27:21 -0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210193136) do
+ActiveRecord::Schema.define(version: 20161212101035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 20161210193136) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["survivor_id"], name: "index_locations_on_survivor_id", using: :btree
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.text     "result"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "survivors", force: :cascade do |t|
